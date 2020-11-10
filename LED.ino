532bf272b84b209f6e642e2313c4823c970ed02d
abdfnx <64256993+abdfnx@users.noreply.github.com>
2020-11-10 16:08:53 +0300

    arduino

---
+++ LED.ino
@@ -0,0 +1,30 @@
+const int LED1 = 13;
+const int LED2 = 11;
+const int LED3 = 9;
+
+void setup() {
+ pinMode(LED1, OUTPUT);
+ pinMode(LED2, OUTPUT);
+ pinMode(LED3, OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(LED1, HIGH);
+ delay(2000);
+
+ digitalWrite(LED2, HIGH);
+ delay(1000);
+ 
+ digitalWrite(LED1, LOW);
+ delay(1000);
+
+ digitalWrite(LED2, LOW);
+ delay(1000);
+ 
+ digitalWrite(LED3, HIGH);
+ delay(2000);
+
+ digitalWrite(LED3, LOW);
+ delay(1000);
+}
