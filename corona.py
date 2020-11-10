21178697062aee6b34a0b3e863dce72d82d348dd
abdfnx <64256993+abdfnx@users.noreply.github.com>
2020-11-10 16:06:11 +0300

    Add files via upload

---
+++ corona.py
@@ -0,0 +1,23 @@
+def eat():
+    pass
+
+def code():
+    pass
+
+def repeat():
+    pass
+
+def sleep():
+    pass
+
+def goOut():
+    pass
+
+def stayAtHome():
+    if (corona.isAlive):
+        eat()
+        code()
+        repeat()
+        sleep()
+    else:
+        goOut()
